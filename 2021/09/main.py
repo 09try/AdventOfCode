65ef61c8d496d6c959749350060d582576619337
09try <0994try@gmail.com>
2021-12-29 16:35:09 +0100

    day 09

--- 2021/09/main.py
+++ 2021/09/main.py
@@ -9,13 +9,38 @@ def get_area(path):
         line = line.replace('\n', '')
         for height in line:
             heights.append(int(height))
-            area.append(heights)
+        area.append(heights)
             
     return area
 
 def get_risk_level(area):
-    low_points = [1, 0, 5, 5]
     
+    low_points = []
+    
+    for row in range(len(area)):
+        for col in range(len(area[row])):
+            current_point = area[row][col]
+            
+            # 0 is lowest and 9 is highest
+            up = 10
+            down = 10
+            left = 10
+            right = 10
+            
+            # adjacent points
+            if row != 0:
+                up = area[row - 1][col]
+            if row != len(area) - 1:
+                down = area[row + 1][col]
+            if col != 0:
+                left = area[row][col - 1]
+            if col != len(area[row]) - 1:
+                right = area[row][col + 1]
+                
+            
+            if current_point < up and current_point < down and current_point < left and current_point < right:
+                low_points.append(current_point)
+        
     
     return sum([x + 1 for x in low_points])
 
