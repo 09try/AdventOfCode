7005f40bd66ea1accadaef6bf78f7a524e3071fe
09try <0994try@gmail.com>
2022-01-01 22:28:26 +0100

    progress with day 11

--- 2021/11/main.py
+++ 2021/11/main.py
@@ -13,55 +13,6 @@ def print_grid(grid):
         r += '\n'
     print(r)
     
-def flash_coordinates(grid):
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] > 9:
-                
-                # update adjacent coords
-                up = False
-                down = False
-                left = False
-                right = False
-
-                # up
-                if row - 1 >= 0:
-                    grid[row - 1][col] += 1
-                    up = True
-                
-                # down
-                if row + 1 < len(grid):
-                    grid[row + 1][col] += 1
-                    down = True
-                
-                # left    
-                if col - 1 >= 0:
-                    grid[row][col - 1] += 1
-                    left = True
-                
-                # right
-                if col + 1 < len(grid[row]):
-                    grid[row][col + 1] += 1
-                    right = True
-                
-                # up left
-                if up and left:
-                    grid[row - 1][col - 1] += 1
-                    
-                # up right
-                if up and right:
-                    grid[row - 1][col + 1] += 1
-                    
-                # down left
-                if down and left:
-                    grid[row + 1][col - 1] += 1
-                
-                # down right
-                if down and right:
-                    grid[row + 1][col + 1] += 1
-                        
-    return grid
-    
 def get_count_of_flashes(grid, steps):
     flashes_count = 0
     
@@ -74,14 +25,105 @@ def get_count_of_flashes(grid, steps):
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 grid[row][col] += 1
-         
+                
+        coords_to_flash = []
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] > 9:
+                    coords_to_flash.append((row, col))   
         
+        flashed_coords = []
+        while coords_to_flash != []:
+            for coord in coords_to_flash:
+                row = coord[0]
+                col = coord[1]
+                coords_to_flash.remove(coord)
+                flashed_coords.append(coord)
+                
+                # update adjacent coords
+                up = False
+                down = False
+                left = False
+                right = False
+                
+                # up
+                if row - 1 >= 0:
+                    grid[row - 1][col] += 1
+                    up = True
+                    
+                    if grid[row - 1][col] > 9:
+                        if (row - 1, col) not in flashed_coords and (row - 1, col) not in coords_to_flash:
+                            coords_to_flash.append((row - 1, col))
+                    
+                # down
+                if row + 1 < len(grid):
+                    grid[row + 1][col] += 1
+                    down = True
+                    
+                    if grid[row + 1][col] > 9:
+                        if (row + 1, col) not in flashed_coords and (row + 1, col) not in coords_to_flash:
+                            coords_to_flash.append((row + 1, col))
+                    
+                # left    
+                if col - 1 >= 0:
+                    grid[row][col - 1] += 1
+                    left = True
+                    
+                    if grid[row][col - 1] > 9:
+                        if (row, col - 1) not in flashed_coords and (row, col - 1) not in coords_to_flash:
+                            coords_to_flash.append((row, col - 1))
+                    
+                # right
+                if col + 1 < len(grid[row]):
+                    grid[row][col + 1] += 1
+                    right = True
+                    
+                    if grid[row][col + 1] > 9:
+                        if (row, col + 1) not in flashed_coords and (row, col + 1) not in coords_to_flash:
+                            coords_to_flash.append((row, col + 1))
+                    
+                # up left
+                if up and left:
+                    grid[row - 1][col - 1] += 1
+                    
+                    if grid[row - 1][col - 1] > 9:
+                        if (row - 1, col - 1) not in flashed_coords and (row - 1, col - 1) not in coords_to_flash:
+                            coords_to_flash.append((row - 1, col - 1))
+                    
+                # up right
+                if up and right:
+                    grid[row - 1][col + 1] += 1
+                    
+                    if grid[row - 1][col + 1] > 9:
+                        if (row - 1, col + 1) not in flashed_coords and (row - 1, col + 1) not in coords_to_flash:
+                            coords_to_flash.append((row - 1, col + 1))
+                    
+                # down left
+                if down and left:
+                    grid[row + 1][col - 1] += 1
+                    
+                    if grid[row + 1][col - 1] > 9:
+                        if (row + 1, col - 1) not in flashed_coords and (row + 1, col - 1) not in coords_to_flash:
+                            coords_to_flash.append((row + 1, col - 1))
+                    
+                # down right
+                if down and right:
+                    grid[row + 1][col + 1] += 1
+                    
+                    if grid[row + 1][col + 1] > 9:
+                        if (row + 1, col + 1) not in flashed_coords and (row + 1, col + 1) not in coords_to_flash:
+                            coords_to_flash.append((row + 1, col + 1))
+                    
+        # niektore ktore nabrali > 9 pocas adjacent update
+        # neupdatovali adjacent coordinates
+                
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] > 9:
                     grid[row][col] = 0
                     flashes_count += 1
                        
+        
         print('after step', step + 1)
         print_grid(grid)
     
@@ -99,7 +141,7 @@ if __name__ == '__main__':
     else:
         print('error')
         
-    # steps = 100
+    # steps = 20
     # expected = 1656
     # flashes_count = get_count_of_flashes(test_grid, steps)
     # print(flashes_count)
