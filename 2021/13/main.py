8f86fa4785eb6c717c710d3e862b556b45a5a89b
09try <0994try@gmail.com>
2022-01-06 09:24:10 +0100

    day 13 progress

--- 2021/13/main.py
+++ 2021/13/main.py
@@ -1,3 +1,6 @@
+from os import error
+
+
 def read_instructions(path):
     f = open(path)
     lines = f.readlines()
@@ -37,14 +40,33 @@ def do_fold(fold_instruction, paper):
         # vertical fold
         first_half = [row[:fold_instruction[1]] for row in paper]
         second_half = [row[fold_instruction[1] + 1:] for row in paper]
-
+        
+        #print(len(first_half[0]))
+        #print(len(second_half[0]))
+        if len(first_half[0]) != len(second_half[0]):
+            # for row in second_half:
+            #     row.append('#')
+                
+            for row in range(len(first_half[0])):
+                first_half[row] = first_half[row][:-1]
+        
+        # for row in range(len(first_half)):
+        #     _col = 0
+        #     for col in range(len(first_half[row]) -1 , -1, -1):
+        #         if first_half[row][col] == '#':
+        #             second_half[row][_col] = '#'
+        #         _col += 1
+                
         for row in range(len(first_half)):
             _col = 0
-            for col in range(len(first_half[0]) - 1, -1, -1):
+            for col in range(len(first_half[row]) -1 , -1, -1):
                 if first_half[row][col] == '#':
-                    second_half[row][_col] = '#'
+                    try:
+                        second_half[row][_col] = '#'
+                    except:
+                        print(error)
                 _col += 1
-            
+        
         paper = second_half
         
         
@@ -54,7 +76,7 @@ def do_fold(fold_instruction, paper):
         second_half = paper[fold_instruction[1] + 1:][:]
         
         _row = len(second_half) - 1
-        for row in range(len(second_half) - 1):
+        for row in range(len(second_half)):
             _col = 0
             for col in range(len(second_half[0])):
                 if second_half[_row][_col] == '#':
@@ -68,6 +90,7 @@ def do_fold(fold_instruction, paper):
 
 if __name__ == '__main__':
     
+    paper = []
     fold_instructions, paper = read_instructions('test_input.txt')
     
     for fold_instruction in fold_instructions:
@@ -75,9 +98,23 @@ if __name__ == '__main__':
         dots_count = sum(len([c for c in row if c == '#']) for row in paper)
         print(dots_count)
         
+    print()        
+        
+    paper = []
     fold_instructions, paper = read_instructions('input.txt')
     
-    for fold_instruction in fold_instructions:
+    first_expected = 602
+    for i, fold_instruction in enumerate(fold_instructions):
         paper = do_fold(fold_instruction, paper)
         dots_count = sum(len([c for c in row if c == '#']) for row in paper)
+        if i == 0:
+            if dots_count == first_expected:
+                print('ok')
+            else:
+                print('error')
         print(dots_count)
+        
+    for row in range(len(paper)):
+        for col in range(len(paper[row])):
+            print(paper[row][col], end=' ')
+        print()
