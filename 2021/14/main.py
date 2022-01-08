9a950d2be3a2932efea058f01dff255aa325615f
09try <0994try@gmail.com>
2022-01-08 20:00:04 +0100

    day 14 first part

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -13,16 +13,62 @@ def read_input(path):
         
     return polymer_template, pair_insertion_rules
 
+def get_count(polymer_template, pair_insertion_rules, number_of_steps):
+    l = len(polymer_template)
+    for step in range(1, number_of_steps + 1):
+        t = ''
+        for i in range(l - 1):
+            pair = polymer_template[i] + polymer_template[i+1]
+            to_be_inserted = pair_insertion_rules[pair]
+            tmp = pair[0] + to_be_inserted + pair[1]
+            t += tmp[:-1]
+        t += tmp[-1]
+        polymer_template = t
+        l = len(polymer_template)
+        if step % 10 == 0:
+            print(step)
+        
+    counts = {}
+    for c in polymer_template:
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+            
+    d = sorted(counts.items(), key=lambda x: x[1])
+    return d[-1][1] - d[0][1]
+
+
 if __name__ == '__main__':
     
     expected = 1588
+    number_of_steps = 10
     polymer_template, pair_insertion_rules = read_input('test_input.txt')
-    
-    for step in range(10):
-        t = ''
-        for i in range(len(polymer_template) - 1):
-            pair = polymer_template[i] + polymer_template[i+1]
-            to_be_inserted = pair_insertion_rules[pair]
-            t += pair[0] + to_be_inserted
-    
-    
+    count = get_count(polymer_template, pair_insertion_rules, number_of_steps)
+    if count == expected:
+        print('ok')
+    else:
+        print('error')
+        
+    expected = 3411
+    number_of_steps = 10
+    polymer_template, pair_insertion_rules = read_input('input.txt')
+    count = get_count(polymer_template, pair_insertion_rules, number_of_steps)
+    if count == expected:
+        print('ok')
+    else:
+        print('error')
+        
+    expected = 2188189693529
+    number_of_steps = 40
+    polymer_template, pair_insertion_rules = read_input('test_input.txt')
+    count = get_count(polymer_template, pair_insertion_rules, number_of_steps)
+    if count == expected:
+        print('ok')
+    else:
+        print('error')
+        
+    number_of_steps = 40
+    polymer_template, pair_insertion_rules = read_input('input.txt')
+    count = get_count(polymer_template, pair_insertion_rules, number_of_steps)
+    print(count)    
